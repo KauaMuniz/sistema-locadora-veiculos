b023aefd75ba9fef7b8b5619d45108ff0cba2f4a
KauaMuniz <kaua.bmuniz@outlook.com>
2026-06-26 09:51:28 -0300

    refactor: aprimorar modelagem e estrutura do banco de dados

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,9 @@
 create database if not exists  sistema_locadora_carros;
 use sistema_locadora_carros;
 
-create table if not exists veiculos(
-placa varchar(8) primary key not null,
+create table if not exists veiculo(
+id_carro int auto_increment primary key not null ,
+placa varchar(8) unique key not null,
 ano year not null,
 modelo varchar(50) not null,
 quant_km_rodados integer not null,
@@ -11,33 +12,35 @@ valor_diaria decimal(10,2) not null,
 categoria_carro varchar(60) not null
 );
 
-create table if not exists funcionarios(
+create table if not exists funcionario(
 id_funcionario integer primary key not null  auto_increment,
 nome_funcionario varchar(60) not null,
 cargo_funcionario varchar(60) not null);
 
-create table if not exists clientes(
-cpf varchar(11) primary key not null,
+create table if not exists cliente(
+id_cliente int auto_increment primary key not null,
+cpf varchar(11) unique not null,
 rg varchar(15)  not null,
 nome_completo varchar(150) not null,
 telefone varchar(15) not null,
-cnh varchar(11) not null
+cnh varchar(11) unique not null
 );
 
 
-create table if not exists locacoes(
+create table if not exists locacao(
 id_locacao integer primary key not null auto_increment,
-cpf varchar(11)  not null,
-placa varchar(8)   not null,
+id_cliente int  not null,
+id_carro int  not null,
 id_funcionario integer not null,
 data_inicio_locacao datetime not null,
 data_prevista_devolucao datetime not null,
 data_devolucao_real datetime,
 valor_diaria_aplicada decimal(10,2) not null,
 valor_total decimal(10,2) not null,
-foreign key (cpf) references clientes(cpf),
-foreign key (id_funcionario) references funcionarios(id_funcionario),
-foreign key (placa) references veiculos(placa)
+status_carro enum('reservada','em_andamento','finalizada','cancelada') not null,
+foreign key (id_cliente) references cliente(id_cliente),
+foreign key (id_funcionario) references funcionario(id_funcionario),
+foreign key (id_carro) references veiculo(id_carro)
 );
 
 
